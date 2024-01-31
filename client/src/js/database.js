ee06dbe3b6c17c6563b5969ae62a9e5454f9a5a1
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-01-31 18:01:35 -0500

    adds put request to jateDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Adds logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('PUT request sent to the jateDB');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+ // Create a connection to the database database and version we want to use.
+ const jateDb = await openDB('note', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = jateDb.transaction(['note'], 'readwrite');
+
+// Open up the desired object store.
+const store = tx.objectStore('note');
+
+ // Use .add method to update existing content
+ const request = store.add({ content: content});
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+//Adds logic for a method that gets all the content from the database
+export const getDb = async () => console.error('GET request sent to the jateDB');
 
 initdb();
